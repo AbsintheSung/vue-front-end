129746421bca69b3794b8211ca1c1789e6c4c1c5
AbsintheSung <h90243768@gmail>
2024-07-19 12:29:31 +0800

    test:樣式斷點測試

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
-<script setup>
-</script>
-
+<script setup></script>
 <template>
   <main>
-   <h1>我是首頁</h1>
+    <h1
+      class="ssm:text-red-950 xxl:text-purple-600 text-6xl sm:text-green-800 md:text-blue-700 lg:text-orange-600 xl:text-yellow-400"
+    >
+      我是首頁
+    </h1>
   </main>
 </template>
