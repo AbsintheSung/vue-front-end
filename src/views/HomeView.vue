1865bd82cb99cc18097974f5be918b0863bc0e56
AbsintheSung <h90243768@gmail.com>
2024-07-19 23:39:40 +0800

    feat:建立active-title組件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HeaderView from '@/layouts/HeaderView.vue'
 import LinkCard from '@/components/LinkCard.vue'
+import ActiveTitle from '@/components/ActiveTitle.vue'
 </script>
 <template>
   <HeaderView />
@@ -8,5 +9,7 @@ import LinkCard from '@/components/LinkCard.vue'
     <section class="container py-6 font-noto">
       <LinkCard />
     </section>
+    <ActiveTitle :leftTitle="'RECENT'" :rightTitle="'近期活動'" />
+    <ActiveTitle :leftTitle="'ONLINE'" :rightTitle="'線上活動'" />
   </main>
 </template>
