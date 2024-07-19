f56e9c4f94321ae72183fa104b44835a355f42d8
AbsintheSung <h90243768@gmail>
2024-07-19 17:26:48 +0800

    feat:整合svg成一隻檔案

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HeaderView from '@/layouts/HeaderView.vue'
+import sprite from '@/assets/svg/icon.svg'
 </script>
 <template>
   <HeaderView />
@@ -8,11 +9,31 @@ import HeaderView from '@/layouts/HeaderView.vue'
       <div
         class="grid grid-flow-dense grid-cols-3 grid-rows-2 md:grid-cols-6 md:grid-rows-1 md:gap-6"
       >
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+        <div>
+          <svg class="h-full w-full">
+            <use :href="`${sprite}#Capa_1`"></use>
+          </svg>
+        </div>
+        <div>
+          <svg class="h-full w-full">
+            <use :href="`${sprite}#Layer_1`"></use>
+          </svg>
+        </div>
+        <div>
+          <svg class="h-full w-full">
+            <use :href="`${sprite}#Capa_2`"></use>
+          </svg>
+        </div>
+        <div>
+          <svg class="h-full w-full">
+            <use :href="`${sprite}#Icon`"></use>
+          </svg>
+        </div>
+        <div>
+          <svg class="h-full w-full">
+            <use :href="`${sprite}#Capa_3`"></use>
+          </svg>
+        </div>
         <div>6</div>
       </div>
     </section>
