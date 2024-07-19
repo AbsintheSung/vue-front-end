5c56e13753cd669a4b6d133fda79eaf22db2db39
AbsintheSung <h90243768@gmail.com>
2024-07-20 01:25:29 +0800

    feat:將商品列表拆成組件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import HeaderView from '@/layouts/HeaderView.vue'
 import LinkCard from '@/components/LinkCard.vue'
 import ActiveTitle from '@/components/ActiveTitle.vue'
+import TickCard from '@/components/TickCard.vue'
 </script>
 <template>
   <HeaderView />
@@ -12,32 +13,7 @@ import ActiveTitle from '@/components/ActiveTitle.vue'
     <ActiveTitle :leftTitle="'RECENT'" :rightTitle="'近期活動'" />
     <ActiveTitle :leftTitle="'ONLINE'" :rightTitle="'線上活動'" />
     <ul class="container grid grid-cols-1 gap-6 py-2 font-noto sm:grid-cols-2 md:grid-cols-3">
-      <li>
-        <div class="flex h-full flex-col">
-          <h2 class="border-b-2 border-black pb-3 font-yeseva text-[32px] font-normal md:text-4xl">
-            19 MAY 2023
-          </h2>
-          <router-link to="/" class="block hover:text-yellow-700">
-            <img class="mt-3" src="../assets/images/01.jpg" alt="活動圖片" />
-            <h3 class="my-3 text-lg font-bold md:text-2xl">邪典電影派對一房間</h3>
-            <p class="mb-4">
-              你有聽說過「大災難家」裡面的主角 Tommy Wiseau
-              製作的「史上最爛的電影」——房間嗎？在美國甚至演變成一種邪教、一種派對的形式，在電影播放的對著螢幕丟湯匙都...
-            </p>
-          </router-link>
-          <div
-            class="mt-auto flex items-center justify-between border-y-2 border-black py-2 font-bold md:py-3"
-          >
-            <router-link to="/" class="flex hover:underline"
-              ><img src="../assets/images/sparkler.png" class="me-2 w-6" />社群活動</router-link
-            >
-
-            <router-link to="/" class="card-hover-pseudo block"
-              >開賣中<FontAwesomeIcon :icon="['fas', 'angles-right']"
-            /></router-link>
-          </div>
-        </div>
-      </li>
+      <TickCard v-for="item in 6" :key="item" />
     </ul>
   </main>
 </template>
