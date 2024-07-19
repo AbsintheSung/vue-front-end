e98beec9bd36fc87b9215d552b382771a4a03769
AbsintheSung <h90243768@gmail>
2024-07-19 16:47:21 +0800

    feat:配置基礎網格與svg檔

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,4 +3,18 @@ import HeaderView from '@/layouts/HeaderView.vue'
 </script>
 <template>
   <HeaderView />
+  <main>
+    <section class="container py-6">
+      <div
+        class="grid grid-flow-dense grid-cols-3 grid-rows-2 md:grid-cols-6 md:grid-rows-1 md:gap-6"
+      >
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    </section>
+  </main>
 </template>
