9fbad5e8fb4792f051b4a8ace0cde92c84c6a9e5
AbsintheSung <h90243768@gmail>
2024-07-19 18:10:11 +0800

    feat:動態生成svg按鈕

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <script setup>
+import { ref } from 'vue'
 import HeaderView from '@/layouts/HeaderView.vue'
 import TheSvg from '@/components/TheSvg.vue'
-const svgData = ['Capa_1', 'Layer_1', 'Capa_2', 'Icon', 'Capa_3']
+const svgData = ref([
+  { name: 'show', path: 'show' },
+  { name: 'fellowship', path: 'fellowship' },
+  { name: 'exhibition', path: 'exhibition' },
+  { name: 'gourmetfood', path: 'gourmetfood' },
+  { name: 'outdoor', path: 'outdoor' }
+])
 </script>
 <template>
   <HeaderView />
   <main>
     <section class="container py-6">
       <div
-        class="grid grid-flow-dense grid-cols-3 grid-rows-2 md:grid-cols-6 md:grid-rows-1 md:gap-6"
+        class="grid grid-flow-dense grid-cols-3 grid-rows-2 gap-2 md:grid-cols-6 md:grid-rows-1 md:gap-6"
       >
-        <div v-for="svgItem in svgData" :key="svgItem">
-          <TheSvg :svgIcon="svgItem" />
+        <div v-for="svgItem in svgData" :key="svgItem.name">
+          <router-link class="blcok">
+            <TheSvg :svgIcon="svgItem.name" />
+          </router-link>
         </div>
-        <div></div>
+        <div class="h-full">
+          <router-link
+            class="flex h-full w-full items-center justify-center bg-black text-5xl font-black text-yellow-400"
+            >探索<br />更多</router-link
+          >
+        </div>
       </div>
     </section>
   </main>
