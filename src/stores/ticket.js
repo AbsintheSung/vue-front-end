0e8b5f7cf3b5f012586deb9c82ca956acaca4829
AbsintheSung <h90243768@gmail.com>
2024-07-20 23:49:28 +0800

    update:調整tick-store內容

--- src/stores/ticket.js
+++ src/stores/ticket.js
@@ -5,23 +5,33 @@ const baseURL = import.meta.env.VITE_APP_API_URL
 const apiName = import.meta.env.VITE_APP_API_NAME
 export const useTicketStore = defineStore('tickets', () => {
   //state
-  const ticketData = ref({}) //初始資料
+  const ticketData = ref({}) //初始篩選資料
+  const ticketAllData = ref({}) //初始全部資料
 
 
   //getter
+  const getTicketAllData = computed(() => ticketAllData.value.products)
   const getTicketData = computed(() => ticketData.value.products)
   const getTicketPage = computed(() => ticketData.value.pagination)
 
   //action
   const fetchAllTicketData = async () => {
     try {
-      const response = await axios(`${baseURL}/v2/api/${apiName}/products`);
-      ticketData.value = response.data;
-      console.log(response)
+      const response = await axios(`${baseURL}/v2/api/${apiName}/products/all`);
+      ticketAllData.value = response.data;
     } catch (error) {
       console.log(error)
     }
   }
+  // const fetchTicketData = async () => {
+  //   try {
+  //     const response = await axios(`${baseURL}/v2/api/${apiName}/products`);
+  //     ticketData.value = response.data;
+  //     console.log(response)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
   const fetchPageInfo = async (page = '', category = '') => {
     try {
       const response = await axios(`${baseURL}/v2/api/${apiName}/products`, {
@@ -36,5 +46,5 @@ export const useTicketStore = defineStore('tickets', () => {
     }
   }
 
-  return { ticketData, getTicketData, fetchAllTicketData, getTicketPage, fetchPageInfo }
+  return { ticketData, getTicketData, getTicketPage, getTicketAllData, fetchPageInfo, fetchAllTicketData }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,11 @@ import LinkCard from '@/components/LinkCard.vue'
 import ActiveTitle from '@/components/ActiveTitle.vue'
 import TickCard from '@/components/TickCard.vue'
 import { useTicketStore } from '@/stores/ticket'
+import { onMounted } from 'vue'
 const tickStore = useTicketStore()
+onMounted(() => {
+  tickStore.fetchAllTicketData()
+})
 </script>
 <template>
   <HeaderView />
@@ -15,7 +19,7 @@ const tickStore = useTicketStore()
     <ActiveTitle :leftTitle="'RECENT'" :rightTitle="'近期活動'" />
     <ActiveTitle :leftTitle="'ONLINE'" :rightTitle="'線上活動'" />
     <ul class="container grid grid-cols-1 gap-6 py-2 font-noto sm:grid-cols-2 md:grid-cols-3">
-      <TickCard v-for="item in tickStore.getTicketData" :key="item.id" :cardItem="item" />
+      <TickCard v-for="item in tickStore.getTicketAllData" :key="item.id" :cardItem="item" />
     </ul>
   </main>
 </template>
