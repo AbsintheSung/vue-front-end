ad9f0ef4c225e020397b788538b1f33e18fa36d7
AbsintheSung <h90243768@gmail.com>
2024-07-20 23:26:17 +0800

    update:註解調App.vue的store設定

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
-import { watch, onMounted } from 'vue'
+// import { watch, onMounted } from 'vue'
 import { RouterView } from 'vue-router'
-import { useTicketStore } from '@/stores/ticket'
-const tickStore = useTicketStore()
+// import { useTicketStore } from '@/stores/ticket'
+// const tickStore = useTicketStore()
 
-watch(
-  () => tickStore.ticketData,
-  (dataState) => {
-    const isEmpty = Object.keys(dataState).length === 0
-    isEmpty ? tickStore.fetchAllTicketData() : null
-  }
-)
-onMounted(async () => {
-  await tickStore.fetchAllTicketData()
-})
+// watch(
+//   () => tickStore.ticketData,
+//   (dataState) => {
+//     const isEmpty = Object.keys(dataState).length === 0
+//     isEmpty ? tickStore.fetchAllTicketData() : null
+//   }
+// )
+// onMounted(async () => {
+//   await tickStore.fetchAllTicketData()
+// })
 </script>
 
 <template>
--- src/views/ActivePgae.vue
+++ src/views/ActivePgae.vue
@@ -2,7 +2,7 @@
 import ActiveTitle from '@/components/ActiveTitle.vue'
 import TickCard from '@/components/TickCard.vue'
 import PaginatePage from '@/components/PaginatePage.vue'
-import { watch } from 'vue'
+import { watch, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useTicketStore } from '@/stores/ticket'
 const router = useRouter()
@@ -19,13 +19,16 @@ watch(
   },
   { immediate: true }
 )
+onMounted(async () => {
+  await tickStore.fetchPageInfo(route.params.pagenum)
+})
 </script>
 <template>
   <ActiveTitle />
   <ul class="container grid grid-cols-1 gap-6 py-2 font-noto sm:grid-cols-2 md:grid-cols-3">
     <TickCard v-for="item in tickStore.getTicketData" :key="item.id" :cardItem="item" />
   </ul>
-  <div class="container">
+  <div class="container flex">
     <PaginatePage :pageInfo="tickStore.getTicketPage" @sendPageNum="handlePages"></PaginatePage>
   </div>
 </template>
