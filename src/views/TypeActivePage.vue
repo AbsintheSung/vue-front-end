0292077a3120dc87abab95109c378c454d221769
AbsintheSung <h90243768@gmail.com>
2024-07-22 00:37:41 +0800

    feat:在單一篩選頁面添加篩選按鈕

--- src/views/TypeActivePage.vue
+++ src/views/TypeActivePage.vue
@@ -1,6 +1,18 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useTicketStore } from '@/stores/ticket'
+import FilterButtin from '@/components/FilterButton.vue'
+const tickStore = useTicketStore()
+const getFilterBtnData = computed(() => {
+  if (!tickStore.getTicketAllData || tickStore.getTicketAllData.length === 0) {
+    return []
+  }
+  return [...new Set(tickStore.getTicketAllData.map((item) => item.category))]
+})
+</script>
 <template>
   <!-- <main class="container"> -->
+  <FilterButtin :getFilterData="getFilterBtnData" />
   <router-view></router-view>
   <!-- </main> -->
 </template>
