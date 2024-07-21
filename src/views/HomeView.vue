f6d7525a6b593983c111c8a7c2e6b7b8d853ac46
AbsintheSung <h90243768@gmail.com>
2024-07-21 23:43:03 +0800

    update:調整LinkCard內的路由導向

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,6 @@ const tickStore = useTicketStore()
       <LinkCard />
     </section>
     <ActiveTitle :leftTitle="'RECENT'" :rightTitle="'近期活動'" />
-    <ActiveTitle :leftTitle="'ONLINE'" :rightTitle="'線上活動'" />
     <ul class="grid grid-cols-1 gap-6 py-2 font-noto sm:grid-cols-2 md:grid-cols-3">
       <TickCard v-for="item in tickStore.getTicketAllData" :key="item.id" :cardItem="item" />
     </ul>
