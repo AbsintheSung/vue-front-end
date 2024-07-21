fa2c61e9d4cc0ab463ad36e8eef86e39bd77a832
AbsintheSung <h90243768@gmail.com>
2024-07-21 10:56:37 +0800

    feat:撰寫fiterButton樣式

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div class="container flex flex-wrap gap-2">
+    <button class="border-2 border-s-4 border-black px-3 py-1 hover:border-s-[#FBFF22]">
+      資訊
+    </button>
+  </div>
+</template>
+<style scoped></style>
--- src/views/ActivePgae.vue
+++ src/views/ActivePgae.vue
@@ -2,12 +2,14 @@
 import ActiveTitle from '@/components/ActiveTitle.vue'
 import TickCard from '@/components/TickCard.vue'
 import PaginatePage from '@/components/PaginatePage.vue'
+import FilterButtin from '@/components/FilterButton.vue'
 import { watch, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useTicketStore } from '@/stores/ticket'
 const router = useRouter()
 const route = useRoute()
 const tickStore = useTicketStore()
+
 const handlePages = async (pageNum) => {
   await tickStore.fetchPageInfo(pageNum)
   router.push({ name: 'Active', params: { pagenum: pageNum } })
@@ -26,6 +28,7 @@ onMounted(async () => {
 <template>
   <div class="flex h-full flex-col">
     <ActiveTitle />
+    <FilterButtin />
     <ul class="container grid grid-cols-1 gap-6 py-2 font-noto sm:grid-cols-2 md:grid-cols-3">
       <TickCard v-for="item in tickStore.getTicketData" :key="item.id" :cardItem="item" />
     </ul>
