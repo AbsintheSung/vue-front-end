3970d85da45b949f5bda56fe4a45ed6c99c2c4a9
AbsintheSung <h90243768@gmail>
2024-07-22 17:24:30 +0800

    update:修改按鈕樣式

--- src/components/cart/CartDetailTable.vue
+++ src/components/cart/CartDetailTable.vue
@@ -42,33 +42,33 @@ const handleDelete = (id) => {
   <table v-if="cartData.lenght != 0" class="w-full px-2 text-left text-sm rtl:text-right">
     <thead class="text-xs uppercase">
       <tr>
-        <th scope="col" class="px-16 py-3">
+        <th scope="col" class="hidden px-16 py-3 md:block">
           <span class="sr-only">Image</span>
         </th>
-        <th scope="col" class="px-4 py-3">商品名稱</th>
-        <th scope="col" class="px-4 py-3">商品數量</th>
-        <th scope="col" class="px-4 py-3">商品價格</th>
-        <th scope="col" class="px-4 py-3">刪除</th>
+        <th scope="col" class="px-2 py-3">商品名稱</th>
+        <th scope="col" class="px-2 py-3">商品數量</th>
+        <th scope="col" class="px-2 py-3">商品價格</th>
+        <th scope="col" class="px-2 py-3">刪除</th>
       </tr>
     </thead>
     <tbody>
       <tr class="border-b border-black" v-for="cartItem in cartData" :key="cartItem.id">
-        <td class="p-4">
+        <td class="hidden p-2 md:block">
           <img
             :src="cartItem.product.imageUrl"
             class="max-h-full w-16 max-w-full md:w-32"
             alt="圖片"
           />
         </td>
-        <td class="px-6 py-4 font-semibold">{{ cartItem.product.title }}</td>
-        <td class="px-6 py-4">
+        <td class="p-2 font-semibold">{{ cartItem.product.title }}</td>
+        <td class="p-2">
           <div class="flex items-center">
             <button
-              class="p-4"
+              class="flex items-center justify-center rounded-full bg-black p-1"
               type="button"
               @click="handleMinusQuenity(cartItem.id, cartItem.qty)"
             >
-              -
+              <FontAwesomeIcon :icon="['fas', 'minus']" class="text-[#FBFF22]" />
             </button>
             <div>
               <!-- <input
@@ -80,15 +80,19 @@ const handleDelete = (id) => {
                 :disabled="true"
                 valu="cartItem.qty"
               /> -->
-              <p class="p-4">{{ cartItem.qty }}</p>
+              <p class="p-2">{{ cartItem.qty }}</p>
             </div>
-            <button class="p-4" type="button" @click="handlePlusQuenity(cartItem.id, cartItem.qty)">
-              +
+            <button
+              class="flex items-center justify-center rounded-full bg-black p-1"
+              type="button"
+              @click="handlePlusQuenity(cartItem.id, cartItem.qty)"
+            >
+              <FontAwesomeIcon :icon="['fas', 'plus']" class="text-[#FBFF22]" />
             </button>
           </div>
         </td>
-        <td class="px-6 py-4 font-semibold">NT$ {{ cartItem.final_total }}</td>
-        <td class="px-6 py-4">
+        <td class="p-2 font-semibold">NT$ {{ cartItem.final_total }}</td>
+        <td class="p-2">
           <button class="font-medium" @click="handleDelete(cartItem.id)">刪除</button>
         </td>
       </tr>
