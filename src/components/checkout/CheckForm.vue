c5c04c5bf1247e5e60b41d2e33c38416262b9424
AbsintheSung <h90243768@gmail>
2024-07-23 11:29:12 +0800

    feat:checkoutForm畫面

---
+++ src/components/checkout/CheckForm.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { userSchema } from '@/plugins/vee-vailbate.config'
+import imgIcon from '@/assets/images/sparkler.png'
+const schema = userSchema
+</script>
+<template>
+  <div class="pb-6 pt-8">
+    <img class="mx-auto" :src="imgIcon" />
+    <h2 class="text-center font-noto text-2xl font-bold">聯絡人資訊</h2>
+  </div>
+  <form class="mx-auto w-full font-noto md:w-2/3">
+    <div class="mb-5">
+      <label for="useremail" class="mb-2 block text-sm font-medium">電子信箱-Email</label>
+      <input
+        type="text"
+        id="useremail"
+        class="block w-full rounded-lg border-2 border-black bg-custom-bg-1 p-2.5 text-sm"
+        placeholder="Bonnie Green"
+      />
+      <p class="mt-2 text-sm font-bold text-red-600">Oops! Username already taken!</p>
+    </div>
+    <div class="mb-5">
+      <label for="username" class="mb-2 block text-sm font-medium">姓名</label>
+      <input
+        type="text"
+        id="username"
+        class="block w-full rounded-lg border-2 border-black bg-custom-bg-1 p-2.5 text-sm"
+        placeholder="Bonnie Green"
+      />
+      <p class="mt-2 text-sm font-bold text-red-600">Oops! Username already taken!</p>
+    </div>
+    <div class="mb-5">
+      <label for="userphone" class="mb-2 block text-sm font-medium">電話</label>
+      <input
+        type="tel"
+        id="userphone"
+        class="block w-full rounded-lg border-2 border-black bg-custom-bg-1 p-2.5 text-sm"
+        placeholder="Bonnie Green"
+      />
+      <p class="mt-2 text-sm font-bold text-red-600">Oops! Username already taken!</p>
+    </div>
+    <div class="mb-5">
+      <label for="useraddress" class="mb-2 block text-sm font-medium">地址</label>
+      <input
+        type="text"
+        id="useraddress"
+        class="block w-full rounded-lg border-2 border-black bg-custom-bg-1 p-2.5 text-sm"
+        placeholder="Bonnie Green"
+      />
+      <p class="mt-2 text-sm font-bold text-red-600">Oops! Username already taken!</p>
+    </div>
+    <div class="mb-5">
+      <label for="usermessage" class="mb-2 block text-sm font-medium">留言</label>
+      <input
+        type="text"
+        id="usermessage"
+        class="block w-full rounded-lg border-2 border-black bg-custom-bg-1 p-2.5 text-sm"
+        placeholder="Bonnie Green"
+      />
+    </div>
+  </form>
+</template>
+<style scoped></style>
--- src/views/CheckoutForm.vue
+++ src/views/CheckoutForm.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate'
-import { userSchema } from '@/plugins/vee-vailbate.config' // 导入验证规则配置
-
-const schema = userSchema
-console.log(schema)
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import CheckTable from '@/components/checkout/CheckTable.vue'
+import CheckForm from '@/components/checkout/CheckForm.vue'
+const baseURL = import.meta.env.VITE_APP_API_URL
+const apiName = import.meta.env.VITE_APP_API_NAME
+const cartData = ref([])
+const priceData = ref({})
+const getCartsData = async () => {
+  const response = await axios(`${baseURL}/v2/api/${apiName}/cart`)
+  const { final_total: finalTotal, total } = response.data.data
+  priceData.value = { final_total: finalTotal, total }
+  cartData.value = response.data.data.carts
+}
+onMounted(() => {
+  getCartsData()
+})
 </script>
 <template>
-  <div></div>
+  <CheckTable :cartData="cartData" :priceData="priceData" />
+  <CheckForm />
 </template>
 <style scoped></style>
