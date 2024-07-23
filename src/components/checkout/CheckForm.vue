f72c4942be02e0a1b682522e011f30cd1ae134d7
AbsintheSung <h90243768@gmail>
2024-07-23 15:12:25 +0800

    feat:串接加入訂單api並重新渲染

--- src/components/checkout/CheckForm.vue
+++ src/components/checkout/CheckForm.vue
@@ -2,8 +2,12 @@
 import { Form, Field, ErrorMessage } from 'vee-validate'
 import { userSchema } from '@/plugins/vee-vailbate.config'
 import { ref } from 'vue'
+import axios from 'axios'
 import imgIcon from '@/assets/images/sparkler.png'
 import FormInput from './FormInput.vue'
+const baseURL = import.meta.env.VITE_APP_API_URL
+const apiName = import.meta.env.VITE_APP_API_NAME
+const schema = userSchema
 const initUserInput = {
   name: '',
   email: '',
@@ -24,17 +28,43 @@ const inputInfo = [
   { title: '電話', type: 'tel', id: 'tel', name: 'userphone', placeholder: 'ex:0900000000' },
   { title: '地址', type: 'text', id: 'address', name: 'useraddress', placeholder: '請輸入聯絡地址' }
 ]
-const fetUserDate = () => {
-  console.log(userInput.value)
+const fetUserDate = async (validate, resetForm) => {
+  const validateRes = await validate()
+  if (!validateRes.valid) return
+  const orderData = {
+    data: {
+      user: {
+        name: userInput.value.name,
+        email: userInput.value.email,
+        tel: userInput.value.tel,
+        address: userInput.value.address
+      },
+      message: userInput.value.message
+    }
+  }
+  try {
+    const response = await axios.post(`${baseURL}/v2/api/${apiName}/order`, orderData)
+    console.log(response)
+    if (response.status === 200) {
+      resetForm()
+      userInput.value = { ...initUserInput } //初始化
+      console.log('添加成功')
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
-const schema = userSchema
 </script>
 <template>
   <div class="pb-6 pt-8">
     <img class="mx-auto" :src="imgIcon" />
     <h2 class="text-center font-noto text-2xl font-bold">聯絡人資訊</h2>
   </div>
-  <Form :validation-schema="schema" class="mx-auto w-full font-noto md:w-2/3">
+  <Form
+    :validation-schema="schema"
+    class="mx-auto w-full font-noto md:w-2/3"
+    v-slot="{ validate, resetForm }"
+  >
     <template v-for="inputItem in inputInfo" :key="inputItem.id">
       <FormInput :inputInfo="inputItem" v-model:userInput="userInput[inputItem.id]" />
     </template>
@@ -97,7 +127,7 @@ const schema = userSchema
       <button
         type="submit"
         class="mx-auto w-full bg-black py-2 text-white md:w-1/2"
-        @click.prevent="fetUserDate"
+        @click.prevent="fetUserDate(validate, resetForm)"
       >
         確認結帳
       </button>
