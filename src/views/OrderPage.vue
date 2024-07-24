f87d524767c5e6811b75da29c4c2040589a40846
AbsintheSung <h90243768@gmail.com>
2024-07-24 17:40:12 +0800

    feat:建立串接單一訂單api

--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const baseURL = import.meta.env.VITE_APP_API_URL
+const apiName = import.meta.env.VITE_APP_API_NAME
+const router = useRoute()
+const orderId = router.params.orderId
+const getOneOrder = async () => {
+  const response = await axios(`${baseURL}/v2/api/${apiName}/order/${orderId}`)
+  console.log(response)
+}
+
+onMounted(() => {
+  getOneOrder()
+})
+console.log(router.params.orderId)
+</script>
 <template>
   <div></div>
 </template>
