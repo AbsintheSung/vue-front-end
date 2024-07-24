88b69d82ecb6b6dc03b2bb34acc2fb49f5956014
AbsintheSung <h90243768@gmail.com>
2024-07-24 18:44:54 +0800

    feat:將orderpage的各表格弄成元件

---
+++ src/components/order/OrderTable.vue
@@ -0,0 +1,38 @@
+<script setup></script>
+<template>
+  <table class="w-full text-left text-sm rtl:text-right">
+    <thead>
+      <tr>
+        <th scope="col" class="hidden px-28 py-3 md:block">
+          <span class="sr-only">Image</span>
+        </th>
+        <th scope="col" class="w-1/2 whitespace-nowrap px-6 py-3">商品名稱</th>
+        <th scope="col" class="w-1/4 whitespace-nowrap px-6 py-3">數量</th>
+        <th scope="col" class="w-1/4 whitespace-nowrap px-6 py-3">價格</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="border-b border-b-slate-700">
+        <td class="hidden px-6 py-4 md:block">
+          <img
+            src="https://github.com/AbsintheSung/7TAO_webpage/blob/main/assets/images/event/02.jpg?raw=true"
+            class="object-cover md:w-full"
+            alt="圖片"
+          />
+        </td>
+        <td scope="row" class="px-6 py-4 font-medium">我是商品名稱</td>
+        <td class="px-6 py-4">123</td>
+        <td class="whitespace-nowrap px-6 py-4">NT$ 3500</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr class="font-semibold">
+        <th scope="row" class="hidden px-6 py-3 text-base md:block"></th>
+        <th scope="row" class="px-6 py-3 text-base">總計</th>
+        <td class="px-6 py-3">4個</td>
+        <td class="whitespace-nowrap px-6 py-3">NT$ 6000</td>
+      </tr>
+    </tfoot>
+  </table>
+</template>
+<style scoped></style>
--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -2,6 +2,9 @@
 import axios from 'axios'
 import { onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import imgIcon from '@/assets/images/sparkler.png'
+import OrderTable from '@/components/order/OrderTable.vue'
+import OrderUserInfo from '@/components/order/OrderUserInfo.vue'
 const baseURL = import.meta.env.VITE_APP_API_URL
 const apiName = import.meta.env.VITE_APP_API_NAME
 const router = useRoute()
@@ -17,6 +20,38 @@ onMounted(() => {
 console.log(router.params.orderId)
 </script>
 <template>
-  <div></div>
+  <div class="pb-6 pt-8">
+    <img class="mx-auto" :src="imgIcon" />
+    <h2 class="text-center font-noto text-2xl font-bold">訂單明細</h2>
+  </div>
+  <OrderTable />
+  <div class="mx-auto my-6 w-full">
+    <!-- <table class="w-full">
+      <tbody>
+        <tr class="border-b">
+          <th class="w-[100px] px-4 py-3 text-left font-semibold text-gray-600">姓名</th>
+          <td class="px-4 py-3 text-center text-gray-900">Absinthe</td>
+        </tr>
+        <tr class="border-b">
+          <th class="px-4 py-3 text-left font-semibold text-gray-600">信箱</th>
+          <td class="px-4 py-3 text-center text-gray-900">h123@gmail.com</td>
+        </tr>
+        <tr class="border-b">
+          <th class="px-4 py-3 text-left font-semibold text-gray-600">聯絡電話</th>
+          <td class="px-4 py-3 text-center text-gray-900">0911-777-888</td>
+        </tr>
+        <tr>
+          <th class="px-4 py-3 text-left font-semibold text-gray-600">聯絡地址</th>
+          <td class="px-4 py-3 text-center text-gray-900">
+            高雄市政府高雄市政府高雄市政府高雄市政府高雄市政府
+          </td>
+        </tr>
+      </tbody>
+    </table> -->
+    <OrderUserInfo />
+    <div class="my-5 flex items-center justify-center">
+      <button class="w-full bg-black py-2 text-white sm:w-fit sm:px-14">確認付款</button>
+    </div>
+  </div>
 </template>
 <style scoped></style>
