76ee53a8321a9e8a165008f0dca11780755bb8f7
AbsintheSung <h90243768@gmail.com>
2024-07-25 13:45:20 +0800

    串接api並渲染

--- src/components/order/OrderTable.vue
+++ src/components/order/OrderTable.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+defineProps({
+  orderList: {
+    type: Object,
+    default: () => ({})
+  },
+  orderPrice: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
 <template>
   <table class="w-full text-left text-sm rtl:text-right">
     <thead>
@@ -10,25 +21,25 @@
       </tr>
     </thead>
     <tbody>
-      <tr class="border-b border-b-slate-700">
+      <tr
+        class="border-b border-b-slate-700"
+        v-for="(itemValue, itemKey) in orderList"
+        :key="itemKey"
+      >
         <td class="hidden px-6 py-4 md:block">
-          <img
-            src="https://github.com/AbsintheSung/7TAO_webpage/blob/main/assets/images/event/02.jpg?raw=true"
-            class="object-cover md:w-full"
-            alt="圖片"
-          />
+          <img :src="itemValue.product.imageUrl" class="object-cover md:w-full" alt="圖片" />
         </td>
-        <td scope="row" class="px-6 py-4 font-medium">我是商品名稱</td>
-        <td class="px-6 py-4">123</td>
-        <td class="whitespace-nowrap px-6 py-4">NT$ 3500</td>
+        <td scope="row" class="px-6 py-4 font-medium">{{ itemValue.product.title }}</td>
+        <td class="px-6 py-4">{{ itemValue.qty }}</td>
+        <td class="whitespace-nowrap px-6 py-4">NT$ {{ itemValue.total }}</td>
       </tr>
     </tbody>
     <tfoot>
       <tr class="font-semibold">
         <th scope="row" class="hidden px-6 py-3 text-base md:block"></th>
         <th scope="row" class="px-6 py-3 text-base">總計</th>
-        <td class="px-6 py-3">4個</td>
-        <td class="whitespace-nowrap px-6 py-3">NT$ 6000</td>
+        <td class="px-6 py-3">{{ orderPrice.qty }}個</td>
+        <td class="whitespace-nowrap px-6 py-3">NT$ {{ orderPrice.total }}</td>
       </tr>
     </tfoot>
   </table>
--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios'
-import { onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import imgIcon from '@/assets/images/sparkler.png'
 import OrderTable from '@/components/order/OrderTable.vue'
@@ -9,9 +9,28 @@ const baseURL = import.meta.env.VITE_APP_API_URL
 const apiName = import.meta.env.VITE_APP_API_NAME
 const router = useRoute()
 const orderId = router.params.orderId
+const orderData = ref({})
+const getOrderList = computed(() => {
+  return orderData.value.products
+})
+const getUserData = computed(() => {
+  return { ...orderData.value.user, is_paid: orderData.value.is_paid }
+})
+const getPrice = computed(() => {
+  if (Object.keys(orderData.value).length === 0) {
+    return {}
+  } else {
+    const totalQty = Object.values(orderData.value.products).reduce((sum, item) => {
+      return sum + (item.qty || 0)
+    }, 0)
+    return { total: orderData.value.total, qty: totalQty }
+  }
+})
+const isOrderPaid = computed(() => orderData.value.is_paid)
 const getOneOrder = async () => {
   const response = await axios(`${baseURL}/v2/api/${apiName}/order/${orderId}`)
   console.log(response)
+  orderData.value = response.data.order
 }
 
 const creatPay = async () => {
@@ -26,40 +45,21 @@ const creatPay = async () => {
 onMounted(() => {
   getOneOrder()
 })
-console.log(router.params.orderId)
 </script>
 <template>
   <div class="pb-6 pt-8">
     <img class="mx-auto" :src="imgIcon" />
     <h2 class="text-center font-noto text-2xl font-bold">訂單明細</h2>
   </div>
-  <OrderTable />
+  <OrderTable :orderList="getOrderList" :orderPrice="getPrice" />
   <div class="mx-auto my-6 w-full">
-    <!-- <table class="w-full">
-      <tbody>
-        <tr class="border-b">
-          <th class="w-[100px] px-4 py-3 text-left font-semibold text-gray-600">姓名</th>
-          <td class="px-4 py-3 text-center text-gray-900">Absinthe</td>
-        </tr>
-        <tr class="border-b">
-          <th class="px-4 py-3 text-left font-semibold text-gray-600">信箱</th>
-          <td class="px-4 py-3 text-center text-gray-900">h123@gmail.com</td>
-        </tr>
-        <tr class="border-b">
-          <th class="px-4 py-3 text-left font-semibold text-gray-600">聯絡電話</th>
-          <td class="px-4 py-3 text-center text-gray-900">0911-777-888</td>
-        </tr>
-        <tr>
-          <th class="px-4 py-3 text-left font-semibold text-gray-600">聯絡地址</th>
-          <td class="px-4 py-3 text-center text-gray-900">
-            高雄市政府高雄市政府高雄市政府高雄市政府高雄市政府
-          </td>
-        </tr>
-      </tbody>
-    </table> -->
-    <OrderUserInfo />
+    <OrderUserInfo :userInfo="getUserData" />
     <div class="my-5 flex items-center justify-center">
-      <button class="w-full bg-black py-2 text-white sm:w-fit sm:px-14" @click="creatPay">
+      <button
+        class="w-full bg-black py-2 text-white sm:w-fit sm:px-14"
+        @click="creatPay"
+        v-if="!isOrderPaid"
+      >
         確認付款
       </button>
     </div>
