948b6037de2fc41a14f43c1667f05282e9a1c46f
AbsintheSung <h90243768@gmail.com>
2024-07-25 12:29:19 +0800

    update:更新OrderListTable排版

--- src/views/OrderListPage.vue
+++ src/views/OrderListPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import axios from 'axios'
 import { useRoute, useRouter } from 'vue-router'
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import OrderListTable from '@/components/orderList/OrderListTable.vue'
 import PaginatePage from '@/components/PaginatePage.vue'
 const baseURL = import.meta.env.VITE_APP_API_URL
@@ -18,7 +18,8 @@ const formatOrderList = computed(() => {
   } else {
     return orderList.value.map((item) => ({
       ...item,
-      create_at: formatTimestamp(item.create_at)
+      create_at: formatTimestamp(item.create_at),
+      paid_date: item.paid_date ? formatTimestamp(item.paid_date) : item.paid_date
     }))
   }
 })
@@ -42,17 +43,27 @@ const formatTimestamp = (timestamp) => {
   const day = String(date.getDate()).padStart(2, '0')
   return `${year}/${month}/${day}`
 }
+
+const handlePages = async (pageNum) => {
+  await getOrderList(pageNum)
+  router.push({ name: 'OrderList', params: { pageNum: pageNum } })
+}
+watch(
+  () => route.params.pageNum,
+  async (paramsNum) => {
+    await getOrderList(paramsNum)
+  },
+  { immediate: true }
+)
 onMounted(() => {
   getOrderList(page)
 })
-const fetchOrderDetail = (orderId) => {
-  router.push(`/orderpay/${orderId}`)
-}
 </script>
 <template>
-  <OrderListTable :orderItem="formatOrderList" @sendOrderId="fetchOrderDetail" />
+  <h2 class="pb-3 pt-1 text-center font-noto text-4xl">訂單明細清單</h2>
+  <OrderListTable :orderItem="formatOrderList" />
 
   <div class="mt-auto flex py-6" v-if="!isPageInfoEmpty">
-    <PaginatePage :pageInfo="pageInfo"></PaginatePage>
+    <PaginatePage :pageInfo="pageInfo" @sendPageNum="handlePages"></PaginatePage>
   </div>
 </template>
