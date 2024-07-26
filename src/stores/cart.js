821c6d1b695536fcf97c99b0e178dda7ec71224c
AbsintheSung <h90243768@gmail.com>
2024-07-26 11:05:31 +0800

    feat:添加加入購物車訊息

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -22,8 +22,11 @@ export const useCartStore = defineStore('cart', () => {
     const fetchAddCart = async (dataInfo) => {
         try {
             const response = await axios.post(`${baseURL}/v2/api/${apiName}/cart`, dataInfo)
+            const successMessage = ref('')
             if (response.status === 200) {
                 await fetchCartData()
+                successMessage.value = response.data.message
+                return successMessage
             }
         } catch (error) {
             console.log(error)
--- src/views/TicketDetail.vue
+++ src/views/TicketDetail.vue
@@ -4,10 +4,14 @@ import ActiveTitle from '@/components/ActiveTitle.vue'
 import CardSwiper from '@/components/CardSwiper.vue'
 import { useTicketStore } from '@/stores/ticket'
 import { computed, onMounted, ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useCartStore } from '@/stores/cart'
+import 'element-plus/es/components/message/style/css' //需手動導入message樣式，否則message會沒有效果
+import { ElMessage } from 'element-plus'
+
 const cartStore = useCartStore()
 const route = useRoute()
+const router = useRouter()
 const tichetId = route.params.ticketId // 獲取路由的id ( 發送獲取資料api需要此id資訊 )
 const baseURL = import.meta.env.VITE_APP_API_URL
 const apiName = import.meta.env.VITE_APP_API_NAME
@@ -66,19 +70,10 @@ const handleTickInfo = async () => {
     }
   }
   isLoading.value = true
-  await cartStore.fetchAddCart(dataInfo)
+  const message = await cartStore.fetchAddCart(dataInfo)
   isLoading.value = false
-  console.log('顯示彈窗，確認是否繼續購物跳轉product頁面或是購物車明細頁面')
-  // try {
-  //   const response = await axios.post(`${baseURL}/v2/api/${apiName}/cart`, dataInfo)
-  //   // console.log(response.status, response.data);
-  //   if (response.status === 200) {
-  //     quenity.value = 1
-  //     console.log(response, '顯示彈窗，確認是否繼續購物跳轉product頁面或是購物車明細頁面')
-  //   }
-  // } catch (error) {
-  //   console.log(error)
-  // }
+  successMes(message)
+  router.push('/active')
 }
 onMounted(async () => {
   isLoading.value = true
@@ -93,6 +88,12 @@ watch(
     isLoading.value = false
   }
 )
+const successMes = (mes = '添加成功') => {
+  ElMessage({
+    message: mes,
+    type: 'success'
+  })
+}
 </script>
 <template>
   <!-- <main class="container"> -->
